77bc82f8d85148f11ff79134687960415fbf7782
akinahmet <ahmtakn@protonmail.com>
2023-12-30 21:37:13 +0100

    add 3 folder

---
+++ module2/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1" # Kullanmak istediğiniz AWS bölgesini belirtin.
+}
+
+resource "aws_instance" "ec2-example" {
+
+  ami = "ami-05c13eab67c5d8861"
+  tags = {"Name" = "ec2-import"}
+  key_name = "mykey"
+  instance_type = "t2.micro"
+  
+}
